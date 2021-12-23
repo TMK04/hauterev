6ee112527b76f807c9e3d05727129c8f655d60b8
floer <floer.learner.01@gmail.com>
2021-12-24 04:31:41 +0800

    refactor(user): remove byUsername function & make user_route a separate router
    Decoupling :>

--- src/routers/users-router/index.ts
+++ src/routers/users-router/index.ts
@@ -1,6 +1,65 @@
-import users_router from "./router";
+import { Router } from "express";
 
-// Side Effects import to configure "/:username" path middleware
-import "./user-route";
+import type { RKRecord } from "types";
+
+import { insertUser } from "database/queries";
+import { userGender } from "database/schemas";
+import { salted_hash } from "helpers";
+import { catchNext, checkBodyProperties } from "routers/helpers";
+
+import user_router from "./user-router";
+
+const users_router = Router();
+
+const rk_password_body = <const>["password"];
+
+/**
+ * Plain text password
+ */
+export type PasswordBody = RKRecord<typeof rk_password_body>;
+
+const rk_login_body = <const>["username", ...rk_password_body];
+
+// type LoginBody = RKRecord<typeof rk_login_body>;
+
+const rk_post_user_body = <const>["email", "last_name", ...rk_login_body];
+
+export interface PostUserBody extends RKRecord<typeof rk_post_user_body> {
+  mobile_number?: string;
+  address?: string;
+  first_name?: string;
+  gender: string;
+}
+
+users_router.post<any, any, any, PostUserBody>(
+  "/",
+  checkBodyProperties(rk_post_user_body, [undefined, null, ""], (key) => `${key} required`),
+  ({ body }, res, next) =>
+    catchNext(async () => {
+      const { username, password, mobile_number, address, email, first_name, last_name, gender } =
+        body;
+      const password_hash = await salted_hash(password);
+
+      try {
+        await insertUser({
+          username,
+          password_hash,
+          mobile_number,
+          address,
+          email,
+          first_name,
+          last_name,
+          gender: userGender(gender),
+          created_timestamp: new Date()
+        });
+      } catch (_) {
+        return res.status(403).send("Username taken");
+      }
+
+      res.sendStatus(201);
+    }, next)
+);
+
+users_router.use("/:username", user_router);
 
 export default users_router;
