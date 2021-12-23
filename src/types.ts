a1d1c332158bcb77a6d5e8027c52348cfb5e16f6
floer <floer.learner.01@gmail.com>
2021-12-23 17:28:12 +0800

    fix(users-router.post): end request if required properties are not provided in the body
    Every property coming from the client side is technically optional, so processing is always needed
    to ensure they are provided.

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,5 @@ export type Key = string | number | symbol;
 export type Optional<T> = {
   [K in keyof T]?: T[K];
 };
+
+export type RKRecord<KA extends readonly string[], T = string> = Record<KA[number], T>;
