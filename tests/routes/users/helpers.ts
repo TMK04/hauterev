9797839e08a06402d8884b51571c3fa449a8c7d1
floer <floer.learner.01@gmail.com>
2022-01-06 13:33:06 +0800

    test(users): add post /users & delete /users/:username tests

---
+++ tests/routes/users/helpers.ts
@@ -0,0 +1,17 @@
+import { post, sserver } from "../../helpers";
+
+export const postBob = () =>
+  post("/users", {
+    username: "Bob123",
+    password: "Bob123Builder",
+    email: "bob_staub_123@mail.com",
+    last_name: "Staub",
+    first_name: "Bob",
+    mobile_number: "12345678",
+    address: "Staub Street 123",
+    gender: "M"
+  });
+
+export const deleteBob = () => sserver.del("/users/Bob123");
+
+export const deleteBobAuthed = () => deleteBob().send({ password: "Bob123Builder" });
