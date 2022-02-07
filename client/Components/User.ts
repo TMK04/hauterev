a2798bf58883203cda9f19e65f50303f3a34c09e
floer <81667533+TMK04@users.noreply.github.com>
2022-02-07 19:37:26 +0800

    feat(user component): add Logout button if username cookie matches User component's username

--- client/Components/User.ts
+++ client/Components/User.ts
@@ -5,7 +5,9 @@ import {
   authorizationHeader,
   createElement,
   get,
+  getUsername,
   goHome,
+  post,
   selectCustomElement,
   utcString,
   whenDefined
@@ -26,9 +28,9 @@ export default class User extends HTMLElement implements AsyncInit {
       const top_header = <TopHeader>selectCustomElement("TopHeader");
       top_header.top_header.innerHTML = `${username}'s Profile`;
     } else return goHome();
-
     const user = await get(`/api/users/${username}`, authorizationHeader());
     if (!user) return goHome();
+
     const {
       email,
       first_name,
@@ -39,18 +41,6 @@ export default class User extends HTMLElement implements AsyncInit {
       created_timestamp,
       reviews
     } = user;
-
-    this.classList.add("d-block", "container", "mx-auto", "mb-4", "bg-light", "rounded-3");
-    const row = createElement("div", [
-      "row",
-      "px-5",
-      "py-4",
-      "row-cols-1",
-      "row-cols-md-2",
-      "row-cols-lg-3",
-      "lg"
-    ]);
-    await whenDefined("BsIcon");
     const details: [string, string, string?][] = [
       [email, "Email", "envelope-fill"],
       [first_name ? `${first_name} ${last_name}` : last_name, "Name"],
@@ -59,9 +49,38 @@ export default class User extends HTMLElement implements AsyncInit {
       [address, "Address", "at"],
       [utcString(created_timestamp), "Joined on", "calendar-event"]
     ];
+
+    this.classList.add("d-block", "container", "mx-auto", "mb-4", "bg-light", "rounded-3", "pb-4");
+    // <div>
+    const row = createElement("div", [
+      "row",
+      "px-5",
+      "pt-4",
+      "pb-3",
+      "row-cols-1",
+      "row-cols-md-2",
+      "row-cols-lg-3",
+      "lg"
+    ]);
+    await whenDefined("BsIcon");
     for (const [content, title, bi] of details)
       if (content) row.append(User.Detail(content, title, bi));
+    // </div>
     this.append(row);
+    if (getUsername() === username) {
+      const logout_row = createElement("div", ["row", "d-flex", "justify-content-center"]);
+      const logout_col = createElement("div", ["col-2", "d-flex", "justify-content-center"]);
+      const logout = createElement("button", ["btn", "btn-warning"]);
+      logout.addEventListener("click", async () => {
+        const res = await post(
+          `/api/users/${username}/auth/logout`,
+          new URLSearchParams(),
+          authorizationHeader()
+        );
+        if (res.ok) location.assign("/index.html");
+      });
+      this.appendChild(logout_row).appendChild(logout_col).appendChild(logout).append("Logout");
+    }
 
     if (reviews) RevCollection.appendToPage(reviews);
   };
